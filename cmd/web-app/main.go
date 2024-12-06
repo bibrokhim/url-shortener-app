05da8c76d87a4bf1f2033aa04c569882b1c3ad52
Bakhromjon <bahromjon.ibrohimov@yandex.com>
2024-12-06 16:40:02 +0500

    add sqlite

--- cmd/web-app/main.go
+++ cmd/web-app/main.go
@@ -1,13 +1,34 @@
 package main
 
 import (
+	"database/sql"
 	"github.com/bibrokhim/url-shortener-app/internal/controllers"
+	"github.com/bibrokhim/url-shortener-app/internal/db"
+	_ "github.com/mattn/go-sqlite3"
 	"log"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/", controllers.ShowIndex)
-	http.HandleFunc("/shorten", controllers.Shorten)
+	sqlite, err := sql.Open("sqlite3", "db.sqlite")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer sqlite.Close()
+
+	if err := db.CreateTable(sqlite); err != nil {
+		log.Fatal(err)
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path == "/" {
+			controllers.ShowIndex(w, r)
+		} else {
+			controllers.Proxy(sqlite)(w, r)
+		}
+	})
+	http.HandleFunc("/shorten", controllers.Shorten(sqlite))
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
--- internal/controllers/shorten.go
+++ internal/controllers/shorten.go
@@ -1,45 +1,71 @@
 package controllers
 
 import (
+	"database/sql"
+	"github.com/bibrokhim/url-shortener-app/internal/db"
 	"github.com/bibrokhim/url-shortener-app/internal/url"
 	"html/template"
 	"net/http"
 	"strings"
 )
 
-func Shorten(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		return
+func Shorten(lite *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+
+		originalURL := r.FormValue("url")
+
+		if originalURL == "" {
+			http.Error(w, "Url required", http.StatusBadRequest)
+			return
+		}
+
+		if !strings.HasPrefix(originalURL, "http://") && !strings.HasPrefix(originalURL, "https://") {
+			originalURL = "https://" + originalURL
+		}
+
+		shortURL := url.ShortenURL(originalURL)
+
+		if err := db.StoreURL(lite, shortURL, originalURL); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		data := map[string]string{
+			"ShortURL": shortURL,
+		}
+
+		t, err := template.ParseFiles("internal/views/shorten.html")
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		if err := t.Execute(w, data); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
 	}
+}
 
-	originalURL := r.FormValue("url")
+func Proxy(lite *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		shortUrl := r.URL.Path[1:]
+		if shortUrl == "" {
+			http.Error(w, "Url required", http.StatusBadRequest)
+			return
+		}
 
-	if originalURL == "" {
-		http.Error(w, "Url required", http.StatusBadRequest)
-		return
+		originalURL, err := db.GetOriginalURL(lite, shortUrl)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusNotFound)
+			return
+		}
+		http.Redirect(w, r, originalURL, http.StatusTemporaryRedirect)
 	}
-
-	if !strings.HasPrefix(originalURL, "http://") && !strings.HasPrefix(originalURL, "https://") {
-		originalURL = "https://" + originalURL
-	}
-
-	shortURL := url.ShortenURL(originalURL)
-
-	data := map[string]string{
-		"ShortURL": shortURL,
-	}
-
-	t, err := template.ParseFiles("internal/views/shorten.html")
-
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	if err := t.Execute(w, data); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
 }
